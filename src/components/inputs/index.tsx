3bc80041ffaea72690b8c489e889e5457f518d5e
jefolidev <jefoliveira279@gmail.com>
2024-11-21 12:09:02 -0300

    botões dos cards agora funcionam

--- src/components/inputs/index.tsx
+++ src/components/inputs/index.tsx
@@ -5,6 +5,7 @@ import {
   InputWrapper,
 } from './styles'
 
+import { useState } from 'react'
 import minusIcon from '../../assets/icons/minus.svg'
 import plusIcon from '../../assets/icons/plus.svg'
 
@@ -17,15 +18,36 @@ export function InputText({ ...rest }: InputProps) {
 }
 
 export function InputNumber({ ...rest }: InputProps) {
+  const [amount, setAmount] = useState(0)
+
+  function handleValueChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const valueInput = Number(e.target.value)
+
+    setAmount(valueInput >= 0 ? valueInput : 0)
+  }
+
+  function handleAddOneToAmount() {
+    setAmount((prevState) => prevState + 1)
+  }
+  function handleRemoveOneToAmount() {
+    setAmount((prevState) => (prevState > 0 ? prevState - 1 : 0))
+  }
+
   return (
     <InputWrapper>
-      <InputNumberButton type="button">
+      <InputNumberButton type="button" onClick={handleRemoveOneToAmount}>
         <img src={minusIcon} alt="" />
       </InputNumberButton>
 
-      <InputNumberContainer type="number" min={0} {...rest} />
+      <InputNumberContainer
+        type="number"
+        value={amount}
+        min={0}
+        {...rest}
+        onChange={handleValueChange}
+      />
 
-      <InputNumberButton type="button">
+      <InputNumberButton type="button" onClick={handleAddOneToAmount}>
         <img src={plusIcon} alt="" />
       </InputNumberButton>
     </InputWrapper>
